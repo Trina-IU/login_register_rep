6527049e739abef95b416214081ede024fdaa70f
Artemixs <roberttacleon@gmail.com>
2024-12-09 14:05:44 -0800

    Database Done

--- app/src/main/java/com/example/medread/MainActivity.java
+++ app/src/main/java/com/example/medread/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.medread;
 
-import android.app.Activity;
+import android.app.Application;
+import com.google.firebase.FirebaseApp;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseApp.initializeApp(this);
+    }
 }
